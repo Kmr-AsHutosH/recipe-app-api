8f51880d997196f5a7cd0815385cc4be342500ae
Kmr-AsHutosH <kmr.ashutosh1112@gmail.com>
2023-04-07 01:43:59 +0530

    updated code

--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,33 @@
 """
 Views for the user API.
 """
-from rest_framework import generics
-from user.serializers import UsersSerializers
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UsersSerializers,
+    AuthTokenSerializer,
+)
+
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UsersSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    """create a new auth token for users."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated users."""
+    serializer_class = UsersSerializers
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated users"""
+        return self.request.user
